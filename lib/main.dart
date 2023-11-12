5e3f17b0a8f5a7c25fe60c8742d6df8d89c9fc1c
Mohamed Khaled <k.abdalrhem@gmail.com>
2023-11-13 01:05:17 +0200

    wsda

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:untitled/LoginScreen.dart';
+import 'package:untitled/Screen/LoginScreen.dart';
 
 import 'firebase_options.dart';
 
