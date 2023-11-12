a8bec696413849c0579cc04886abf2d6dc4c1765
Mohamed Khaled <k.abdalrhem@gmail.com>
2023-11-12 02:30:50 +0200

    new

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,43 +15,64 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          Image.asset('Images/Pattern (2).png'),
-          OutlinedButton(
-            onPressed: () {},
-            child: ListTile(
-              title: Text(
-                'Log in with Google',
-                style: GoogleFonts.dmSans(
-                  textStyle: TextStyle(
-                      color: Color(0xffB2B3B2),
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.w500),
-                ),
-              ),
-              subtitle:Row(
-                children: [
-                  Text('Mohamed khaled',
-                      style: GoogleFonts.dmSans(
-                      textStyle: TextStyle(
-                      color: Color(0xffFFFFFF),
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.w500,
-
-                      ),
-
-                      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset('Images/Pattern (2).png'),
+            OutlinedButton(
+              onPressed: () async {
+                await signInWithGoogle();
+              },
+              child: ListTile(
+                title: Text(
+                  'Log in with Google',
+                  style: GoogleFonts.dmSans(
+                    textStyle: TextStyle(
+                        color: Color(0xffB2B3B2),
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w500),
                   ),
-Image.asset("Images/Verified.png"),
-
-                ],
-              ) ,
-              trailing: Image.asset('Images/google-logo-9808.png'),
+                ),
+                subtitle: Row(
+                  children: [
+                    Text(
+                      'Mohamed khaled',
+                      style: GoogleFonts.dmSans(
+                        textStyle: TextStyle(
+                          color: Color(0xffFFFFFF),
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    Image.asset("Images/Verified.png"),
+                  ],
+                ),
+                trailing: Image.asset('Images/google-logo-9808.png'),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser =
+        await GoogleSignIn(scopes: ['profile', 'email']).signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth =
+        await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
